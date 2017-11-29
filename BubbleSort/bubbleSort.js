f5ffc531cb8c74482e3fd29ea6f355cdb5bfbebc
Jeremy Valance <jvalance@samsung-sm-g930v.bds-ad.lc>
2017-11-29 10:46:28 -0500

    Added bubble sort

---
+++ BubbleSort/bubbleSort.js
@@ -0,0 +1,23 @@
+const bubbleSort = (arr) => {
+  let arrayLength = arr.length;
+
+  let swapped;
+
+  do {
+    swapped = false;
+    for (let i = 0; i < arrayLength; i++;) {
+      if (arr[i] > arr[i + 1]) {
+        let temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+
+  return arr;
+}
+
+let testArray = [8, 3, 7, 1, 3];
+
+bubbleSort(testArray);
