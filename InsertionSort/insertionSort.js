b871b48592665d79e43999b27de7ff4bb7486911
Jeremy Valance <jvalance@samsung-sm-g930v.bds-ad.lc>
2017-11-29 11:48:39 -0500

    added insertion sort

---
+++ InsertionSort/insertionSort.js
@@ -0,0 +1,22 @@
+const insertionSort = (arr) => {
+    let arrayLength = arr.length;
+  
+    for (let i = 1; i < arrayLength; i++) {
+      
+      let key = arr[i];
+      let j = i - 1;
+      
+      while (arr[j] > key) {
+        arr[j + 1] = arr[j];
+        j = j -1;
+      }
+      
+      arr[j + 1] = key;
+    }
+    
+    return arr;
+  }
+  
+  let testArray = [4, 3, 1, 13, 12, 4]
+  
+  insertionSort(testArray);
